2a59c49923cdd483c2c3c09f5e4c3cbfe0eb8126
Aditya Jamuar <adityaj@geekyants.com>
2021-12-14 22:44:21 +0530

    feat: sorting feature in array implemented

--- src/components/CountryStats/index.tsx
+++ src/components/CountryStats/index.tsx
@@ -1,22 +1,123 @@
-import ICountry from "../../types/ICountry";
 import "./index.css";
+import { useMemo, useState } from "react";
+import ICountry from "../../types/ICountry";
+import { BiSortAlt2 } from "react-icons/bi";
 
 const CountryStats = ({ countryArray }: { countryArray: Array<ICountry> }) => {
+  const [sortingColumnName, setSortingColumnName] =
+    useState<keyof ICountry>("Country");
+  const [sortingOrder, setSortingOrder] = useState<"Ascending" | "Descending">(
+    "Ascending"
+  );
+
+  const sortedArray = useMemo(() => {
+    return [...countryArray].sort(function (
+      country1: ICountry,
+      country2: ICountry
+    ) {
+      let sortValue1 = country1[sortingColumnName];
+      let sortValue2 = country2[sortingColumnName];
+
+      if (sortValue1 < sortValue2) return sortingOrder === "Ascending" ? -1 : 1;
+      if (sortValue1 > sortValue2) return sortingOrder === "Ascending" ? 1 : -1;
+      return 0;
+    });
+  }, [countryArray, sortingColumnName, sortingOrder]);
+
+  const toggleSortingOrder = () => {
+    if (sortingOrder === "Ascending") setSortingOrder("Descending");
+    else setSortingOrder("Ascending");
+  };
+
+  const setSorting = (propertyName: keyof ICountry) => {
+    if (sortingColumnName === propertyName) toggleSortingOrder();
+    else {
+      setSortingColumnName(propertyName);
+    }
+  };
+
   return (
     <table id="countries">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>New Confirmed</th>
-          <th>Total Confirmed</th>
-          <th>New Deaths</th>
-          <th>Total Deaths</th>
-          <th>New Recovered</th>
-          <th>Total Recovered</th>
+          <th>
+            <div className="th-spacer">
+              Name{" "}
+              <span className="sort-icon" onClick={() => setSorting("Country")}>
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
+          <th>
+            <div className="th-spacer">
+              New Confirmed{" "}
+              <span
+                className="sort-icon"
+                onClick={() => setSorting("NewConfirmed")}
+              >
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
+          <th>
+            <div className="th-spacer">
+              Total Confirmed{" "}
+              <span
+                className="sort-icon"
+                onClick={() => setSorting("TotalConfirmed")}
+              >
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
+          <th>
+            <div className="th-spacer">
+              New Deaths{" "}
+              <span
+                className="sort-icon"
+                onClick={() => setSorting("NewDeaths")}
+              >
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
+          <th>
+            <div className="th-spacer">
+              Total Deaths{" "}
+              <span
+                className="sort-icon"
+                onClick={() => setSorting("TotalDeaths")}
+              >
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
+          <th>
+            <div className="th-spacer">
+              New Recovered{" "}
+              <span
+                className="sort-icon"
+                onClick={() => setSorting("NewRecovered")}
+              >
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
+          <th>
+            <div className="th-spacer">
+              Total Recovered{" "}
+              <span
+                className="sort-icon"
+                onClick={() => setSorting("TotalRecovered")}
+              >
+                <BiSortAlt2 />
+              </span>
+            </div>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {countryArray.map((country) => {
+        {sortedArray.map((country) => {
           return (
             <tr key={country.Country}>
               <td>{country.Country}</td>
